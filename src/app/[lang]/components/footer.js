e240c9b1c92abd52bc7395faa5919cb3d580764a
vvbbnn00 <vvbbnn00@foxmail.com>
2024-12-15 19:44:50 +0800

    feat: add ICP registration number to footer

--- src/app/[lang]/components/footer.js
+++ src/app/[lang]/components/footer.js
@@ -5,6 +5,7 @@ export default function Footer(){
         <footer>
             <p>Powered By <a href="https://github.com/vvbbnn00/Proj-Homo-Next">Biogas Engine</a>.</p>
             <p>Made with ❤️ by <del>田所浩二</del>{"  "}<a href="https://www.vvbbnn00.cn">vvbbnn00</a></p>
+            <p className="mt-1 text-sm"><a href="https://icp.gov.moe/?keyword=20245411" target="_blank">萌ICP备20245411号</a></p>
         </footer>
     )
 }
